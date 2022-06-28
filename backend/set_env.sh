f322a4977ec1bc78172eee5e58a8eb24f7bb2b1a
zkhater <zkhater@Ziad-HP.localdomain>
2022-06-28 02:53:53 +0200

    Deploy Infra Stage Added

---
+++ backend/set_env.sh
@@ -0,0 +1,24 @@
+# export NODE_ENV=production
+# export VERSION=1
+# export TYPEORM_CONNECTION=postgres
+# export TYPEORM_MIGRATIONS_DIR=./migrations
+# export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+# export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+# export TYPEORM_HOST=localhost
+# export TYPEORM_PORT=5532
+# export TYPEORM_USERNAME=postgres
+# export TYPEORM_PASSWORD=password
+# export TYPEORM_DATABASE=glee
+
+
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_HOST=ziad-rds.cy4dk5arsimv.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=QP13U4yN7EaKsWhtHYF2
+export TYPEORM_DATABASE=ziaddb
